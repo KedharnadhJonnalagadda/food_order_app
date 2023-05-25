00e449576b10f1e306665455e6cf4c189003fcfd
kedharnadh555 <kedharnadh555@gmail.com>
2023-05-25 21:57:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState} from 'react';
+import Cart from './Components/Cart/Cart';
+import Header from './Components/Layout/Header';
+import Meals from './Components/Meals/Meals';
+import CartProvider from './store/CartProvider';
+const App=()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [cartIsShown,setCartIsShown]=useState(false);
 
+  const showCartHandelr=()=>{
+    setCartIsShown(true);
+  }
+  const hideCartHandelr=()=>{
+    setCartIsShown(false);
+
+  }
+
+  return (<CartProvider>
+
+    {cartIsShown && <Cart onHideCart={hideCartHandelr} key={234}/>} 
+    <Header onShowCart={showCartHandelr} />
+   <main>
+    <Meals />
+   </main>
+  </CartProvider>);
+};
 export default App;
---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvalibleMeals";
+import { Fragment } from "react";
+const Meals =()=>{
+ return (<Fragment>
+    <MealsSummary />
+    <AvailableMeals />
+ </Fragment>)
+}
+
+export default Meals;
